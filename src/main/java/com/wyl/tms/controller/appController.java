0f62a93003652083e4ecb634f876ea4bf3637c65
wyl <514320473@qq.com>
2019-04-11 09:52:37 +0800

    上传代码

---
+++ src/main/java/com/wyl/tms/controller/appController.java
@@ -0,0 +1,78 @@
+package com.wyl.tms.controller;
+
+import com.google.code.kaptcha.impl.DefaultKaptcha;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayOutputStream;
+
+@RequestMapping("/tms")
+@Controller
+public class appController {
+
+    @Autowired
+    DefaultKaptcha defaultKaptcha;
+
+    @RequestMapping("/index")
+    public  String index(){
+        return "/home/index";
+    }
+
+    @RequestMapping("/market")
+    public  String foot(){
+        return "/market/market";
+    }
+    @RequestMapping("/personal")
+    public  String personal(){
+        return "/personal/personal";
+    }
+
+
+    @RequestMapping("/setting")
+    public  String login(){
+        return "/personal/setting";
+    }
+
+    @RequestMapping("/register")
+    public  String register(){
+        return "/register";
+    }
+
+    @RequestMapping("/defaultKaptcha")
+    public void defaultKaptcha(HttpServletRequest httpServletRequest, HttpServletResponse httpServletResponse) throws Exception{
+        byte[] captchaChallengeAsJpeg = null;
+        ByteArrayOutputStream jpegOutputStream = new ByteArrayOutputStream();
+        try {
+            //生产验证码字符串并保存到session中
+            String createText = defaultKaptcha.createText();
+            httpServletRequest.getSession().setAttribute("vrifyCode", createText);
+            //使用生产的验证码字符串返回一个BufferedImage对象并转为byte写入到byte数组中
+            BufferedImage challenge = defaultKaptcha.createImage(createText);
+            ImageIO.write(challenge, "jpg", jpegOutputStream);
+        } catch (IllegalArgumentException e) {
+            httpServletResponse.sendError(HttpServletResponse.SC_NOT_FOUND);
+            return;
+        }
+
+        //定义response输出类型为image/jpeg类型，使用response输出流输出图片的byte数组
+        captchaChallengeAsJpeg = jpegOutputStream.toByteArray();
+        httpServletResponse.setHeader("Cache-Control", "no-store");
+        httpServletResponse.setHeader("Pragma", "no-cache");
+        httpServletResponse.setDateHeader("Expires", 0);
+        httpServletResponse.setContentType("image/jpeg");
+        ServletOutputStream responseOutputStream =
+                httpServletResponse.getOutputStream();
+        responseOutputStream.write(captchaChallengeAsJpeg);
+        responseOutputStream.flush();
+        responseOutputStream.close();
+    }
+}
+
+
+
